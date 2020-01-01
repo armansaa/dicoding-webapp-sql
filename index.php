091bd5855fe0e29dfb5520c6c9f06d459b1ba202
armansaa <armansa95@gmail.com>
2020-01-01 22:49:29 +0700

    fix connection to db

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dbsubmission.database.windows.net";
+    $user = "lenovo";
+    $pass = "5ubMi$$i0n";
+    $db = "Registration";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
